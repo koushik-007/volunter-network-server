b3458017b8af5dce744829280cb29c2255e4d3d2
koushik <kawsarmollik5@gmail.com>
2020-10-08 23:45:48 +0600

    done

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ client.connect(err => {
     })
   })
   app.get('/eventDetails/:id', (req,res) =>{
-    eventsCollection.find({id: req.query.id})
+    eventsCollection.find({id: req.params.id})
     .toArray((err, documents) =>{
       res.send(documents)
     })
